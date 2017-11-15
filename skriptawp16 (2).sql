b25aee2f5cbcc095fbd2545d0fd4e62220bd849c
Ana Maria Anic <31928677+AnaMariaAnic@users.noreply.github.com>
2017-11-15 18:20:38 +0100

    skriptawp16 update
    update

--- skriptawp16 (2).sql
+++ skriptawp16 (2).sql
@@ -4,44 +4,52 @@ create database edunovawp16;
 use edunovawp16;
 
 create table smjer(
-sifra int,
-naziv varchar(50),
-cijena decimal(18,2),
-upisnina decimal(18,2),
-brojsati int
+sifra int  not null primary key auto_increment,
+naziv varchar(50) not null,
+cijena decimal(18,2) not null,
+upisnina decimal(18, 2) not null,
+brojsati int not null
 );
 
 create table grupa(
-sifra int,
-smjer int,
-naziv varchar(50),
-predavac int,
-datumpocetka datetime
+sifra int not null primary key auto_increment,
+smjer int not null,
+naziv varchar(50) not null,
+predavac int not null,
+datumpocetka datetime 
 );
 
 create table predavac(
-sifra int,
-osoba char(11),
-placa decimal(18,2)
+sifra int not null primary key auto_increment,
+osoba char(11) not null,
+placa decimal(18, 2)
 );
 
 create table polaznik(
-sifra int,
-osoba char(11),
-brojugovora varchar(20)
+sifra int not null primary key auto_increment,
+osoba char(11) not null,
+brojugovora varchar(20) not null
 );
 
 create table osoba(
-oib char(11),
-ime varchar(50),
-prezime varchar(50),
+oib char(11) not null primary key,
 email varchar(100),
+ime varchar(50) not null default 'Ana', 
+prezime varchar(50) not  null,
 spol boolean
 );
 
 create table clan(
-grupa int,
-polaznik int
+grupa int not null,
+polaznik int not null
 );
 
+alter table grupa add foreign key (smjer) references smjer(sifra);
+alter table grupa add foreign key (predavac) references predavac(sifra);
 
+alter table predavac add foreign key (osoba) references osoba(oib);
+
+alter table polaznik add foreign key (osoba) references osoba(oib);
+
+alter table clan add foreign key (grupa) references grupa(sifra);
+alter table clan add foreign key (polaznik) references polaznik(sifra);
